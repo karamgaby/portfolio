21f6a9793aea6e61769932934fdeff3fb603b2a8
Gaby karam <gaby@codi.tech>
2019-03-17 16:32:08 +0200

    updating the portfolio to the latest achievments

--- gruntfile.js
+++ gruntfile.js
@@ -1,60 +1,60 @@
 module.exports = function (grunt) {
-    var serveStatic = require('serve-static');
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        watch: {
-            options: {
-                livereload: true
-            },
-            jade: {
-                tasks: ["jade:debug"],
-                files: ["**/*.jade", "**/*.md"]
-            }
-        },
-        jade: {
-            options: {
-                pretty: true,
-                files: {
-                    "*": ["**/*.jade", "!layouts/*.jade", "!layouts/*/*.jade"]
-                }
-            },
-            debug: {
-                options: {
-                    locals: {
-                        livereload: true
-                    }
-                }
-            },
-            publish: {
-                options: {
-                    locals: {
-                        livereload: true
-                    }
-                }
-            }
-        },
-        web: {
-            options: {
-                port: 8001
-            }
+  var serveStatic = require('serve-static');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      options: {
+        livereload: true
+      },
+      jade: {
+        tasks: ["jade:debug"],
+        files: ["**/*.jade", "**/*.md"]
+      }
+    },
+    jade: {
+      options: {
+        pretty: true,
+        files: {
+          "*": ["**/*.jade", "!layouts/*.jade", "!layouts/*/*.jade"]
         }
-    });
+      },
+      debug: {
+        options: {
+          locals: {
+            livereload: true
+          }
+        }
+      },
+      publish: {
+        options: {
+          locals: {
+            livereload: true
+          }
+        }
+      }
+    },
+    web: {
+      options: {
+        port: 8001
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-jade-tasks');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jade-tasks');
 
-    grunt.registerTask('default', ['jade:debug', 'web']);
-    grunt.registerTask('publish', ['jade:publish']);
+  grunt.registerTask('default', ['jade:debug', 'web']);
+  grunt.registerTask('publish', ['jade:publish']);
 
-    grunt.registerTask('web', 'Start web server...', function () {
-        var options = this.options();
-        var connect = require('connect');
-        serveStatic('.tmp');
-        connect().use('/bower_components', serveStatic('./bower_components'));
+  grunt.registerTask('web', 'Start web server...', function () {
+    var options = this.options();
+    var connect = require('connect');
+    serveStatic('.tmp');
+    connect().use('/bower_components', serveStatic('./bower_components'));
 
-        console.log('http://localhost:%s', options.port);
+    console.log('http://localhost:%s', options.port);
 
-        grunt.task.run(["watch:jade"]);
-    });
+    grunt.task.run(["watch:jade"]);
+  });
 
 };
